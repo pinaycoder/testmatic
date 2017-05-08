bd718e2f63b82aa29021021a91785dff4561c18a
Joy M <pinaycoder@gmail.com>
2017-05-08 17:51:12 +0800

    Modified database.php to change mysql config

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Eloquent::unguard();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(UsersTableSeeder::class);
         $this->call(TemplatesTableSeeder::class);
         $this->call(TemplateComponentsTableSeeder::class);
         $this->call(ProjectsTableSeeder::class);
         $this->call(ProjectComponentsTableSeeder::class);
         $this->call(SecurityQuestionsTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
