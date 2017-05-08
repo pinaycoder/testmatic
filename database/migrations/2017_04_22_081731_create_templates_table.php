11317f567541f1ce8732b3a21d35c6d871e71402
Joy M <pinaycoder@gmail.com>
2017-05-08 19:29:54 +0800

    Modified seeder classes and controllers to handle the change for created_date and modified_date columns

--- database/migrations/2017_04_22_081731_create_templates_table.php
+++ database/migrations/2017_04_22_081731_create_templates_table.php
@@ -18,8 +18,8 @@ class CreateTemplatesTable extends Migration
             $table->string('name', 20)->default('');
             $table->text('description');
             $table->string('entry_url')->default('');
-            $table->integer('created_by')->unsigned()->default(1);
-            $table->foreign('created_by')->references('id')->on('users');
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamp('created_date')->nullable()->default(NULL);
             $table->timestamp('modified_date')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->integer('modified_by')->unsigned()->default(1);
