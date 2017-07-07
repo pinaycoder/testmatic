41c79dca804ec354585e4ec3e527e03f98ec18ff
pinaycoder <pinaycoder@gmail.com>
2017-07-08 01:01:32 +0800

    Sprint 2

---
+++ resources/views/layouts/testing-complete.blade.php
@@ -0,0 +1,18 @@
+<div class="row gray-bg testing-welcome jumbotron">
+    <div class="col-lg-12" style="margin: 0 auto; text-align: center;">
+		<p>Thank you for taking the time to complete this test!</p>
+        <p>Click 'Exit' to go back to projects list.</p>
+	</div>
+</div>
+
+<div class="row testing-footer">
+    <div class="col-lg-2">
+        &nbsp;
+    </div>
+    <div class="col-lg-8">
+        &nbsp;
+    </div>
+    <div class="col-lg-2">
+        <a href="/projects" class="btn btn-sm btn-default btn-block">Exit</a>
+    </div>
+</div>
